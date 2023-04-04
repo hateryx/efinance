e99168b1f9df06e1f4d5ad83c60f051b81b5f0c5
Richmond S. Damag <112183555+hateryx@users.noreply.github.com>
2023-04-04 09:30:20 +0000

    Minor updates

--- app.py
+++ app.py
@@ -32,7 +32,6 @@ with fin_app.app_context():
     conn = db.engine.connect()
     db.create_all()
 
-
 class users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), nullable=False)
